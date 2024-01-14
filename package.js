c309262cf4b9cbd1dbbf819df7a8b2feb11c4b08
cem1000 <55397332+cem1000@users.noreply.github.com>
2024-01-14 23:15:22 +0000

    Update package.js

--- package.js
+++ package.js
@@ -4,16 +4,15 @@
   "description": "API for Dublin Bikes availability",
   "main": "app.js",
   "scripts": {
-    "start": "node app
-.js"
-},
-"dependencies": {
-"express": "^4.17.1",
-"node-fetch": "^2.6.1",
-"csvtojson": "^2.0.10"
-},
-"engines": {
-"node": ">=14"
-},
-"license": "MIT"
-}
+    "start": "node app.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1",
+    "node-fetch": "^2.6.1",
+    "csvtojson": "^2.0.10"
+  },
+  "engines": {
+    "node": ">=14"
+    },
+    "license": "MIT"
+    }
